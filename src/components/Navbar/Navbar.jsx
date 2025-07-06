72a3e8490db949d098edc8cf54cc8cf7e83d6101
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2025-07-06 19:27:04 +0600

    updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { Button } from "../ui/button";
 
 export default function Navbar() {
   return (
-    <div className="max-w-[1400px] mx-auto px-4 flex justify-between items-center">
-      <div className="text-2xl font-bold mr-10">Navbar</div>
-      <div>
-        <Button variant="outline">Login</Button>
-      </div>
+    <div className="max-w-[1400px] mx-auto px-4 ">
+     <h1>Navbar</h1>
+      
     </div>
   );
 }
